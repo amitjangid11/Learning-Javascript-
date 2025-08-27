28cc5660243682fc299fbdfec7d1750f6ee6b246
amitjangid11 <amitjangid.aj11@gmail.com>
2025-08-28 05:23:17 +0530

    JSON and API

--- 2. Part-2/03_objects2.js
+++ 2. Part-2/03_objects2.js
@@ -2,5 +2,51 @@
 const tinderUser = {}
 tinderUser.id = "123abc";
 tinderUser.name = "Sammy";
+tinderUser.isLoggedIn = false;
 
-console.log(tinderUser);
+// console.log(tinderUser);
+
+const regularUSer = {
+    email: "some@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "Amit",
+            lastname: "Jangid"
+        }
+    },
+    address: "Jaipur"
+}
+
+console.log(regularUSer.fullname.userfullname.firstname);
+
+const obj1 = { 1: "a", 2: "b" };
+const obj2 = { 3: "a", 4: "b" };
+const obj4 = { 5: "a", 6: "b" };
+
+// const obj3 = { obj1, obj2 };
+// const obj3 = Object.assign({}, obj1, obj2,obj4);
+const obj3 = { ...obj1, ...obj2 }
+console.log(obj3);
+
+const users = [
+    {
+        id: 1,
+        email: "abc@gmail.com"
+    },
+    {
+        id: 2,
+        email: "bcd@gmail.com"
+    },
+    {
+        id: 3,
+        email: "cde@gmail.com"
+    }
+]
+console.log(users[1].email);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('email'));//false
+console.log(tinderUser.hasOwnProperty('id'));// true
---
+++ 2. Part-2/04_object_Destructuring.js
@@ -0,0 +1,16 @@
+const course = {
+    courseName: "Js in Hindi",
+    coursePrice: "999",
+    courseInstructor: "Hitesh Chaudhary"
+}
+
+const { courseInstructor: instrctor } = course;
+console.log(courseInstructor);
+
+// Destructuring
+const navbar = ({ company }) => {
+
+}
+navbar(company = "hitesh");
+
+// Next topic was json and api 
